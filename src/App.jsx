c5406f982882d03a28bd2da5138dd5e9dcfcb752
snerualll <laurens_rijnja@hotmail.com>
2025-07-07 11:54:30 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import "./index.css";
 export default function App() {
   return (
     <div style={{ minHeight: "100vh", background: "var(--color-bg)" }}>
+      <div>Dit is een test</div>
       <Board />
     </div>
   );
